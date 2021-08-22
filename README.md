673023738ce90769c667ac23a65c3badad36f2f0
VanessaLin9 <vanessa7591@gmail.com>
2021-08-22 09:28:33 +0800

    feat: add passport and readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# TodoList-sequelize
+----------------------------
+由Express製作的備忘錄
+模板引擎使用handlebars
+使用sequelize搭配/MySQL資料庫
+***
+## Installation-安裝說明
+
+1. clone本專案
+2. 本專案使用MySQL 請確認安裝
+3. npm i 
+4. 啟動專案 npm run dev
+5. 開啟瀏覽器, 輸入網址 [http://localhost:3000](http://localhost:3000)
+
+***
+## Features-專案功能
+
++ 使用者可以註冊帳號, 登入帳號
++ 使用者可以在todo list 中檢視項目
++ 使用者可以在todo list 中增加項目
++ 使用者可以在todo list 中修改項目
++ 使用者可以在todo list 中刪除項目
--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const usePassport = require('./config/passport') //載入一包 Passport 設定檔
+// 引用 passport，放在文件上方
+const passport = require('passport') //把 Passport 套件本身載入進來
 
 const db = require('./models')
 const Todo = db.Todo
@@ -13,8 +17,20 @@ const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
+
+// 加入 middleware，驗證 reqest 登入狀態
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+})) //Passport 套件功能
 
 app.get('/', (req, res) => {
   return Todo.findAll({
